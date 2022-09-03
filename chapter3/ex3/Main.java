6c4d3e1a4dc6a76beb89fd7ae6f2642a2b75c83f
hiroaki-yamada <manigeo273511@gmail.com>
2022-09-03 15:18:42 +0900

    try-finally は可能だよ

---
+++ chapter3/ex3/Main.java
@@ -0,0 +1,12 @@
+package chapter3.ex3;
+
+public class Main {
+    public static void main(String[] args) {
+        // java でも try-finally は可能でした。
+        try {
+
+        } finally {
+
+        }
+    }
+}
